125802f9b3a1f0af3799773be0bb2ba81ccbab1d
codetivated <melisa@heying.us>
2025-10-24 08:48:39 -0500

    wireframe

---
+++ src/app/pages/add-recipe/add-recipe.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-recipe',
+  imports: [],
+  templateUrl: './add-recipe.html',
+  styleUrl: './add-recipe.scss',
+})
+export class AddRecipe {
+
+}
